456158667a74a2cf2d367e3aa1e0609b67e9a0fd
TATSUNO “Taz” Yasuhiro <ytatsuno.jp@gmail.com>
2025-04-16 21:57:19 +0900

    Docs (#13)

--- cmd/generate.go
+++ cmd/generate.go
@@ -8,7 +8,16 @@ import (
 )
 
 var generateCommand = &cobra.Command{
-	Use: "generate",
+	Use:   "generate [PATH]",
+	Short: "Generate libs.versions.toml",
+	Long: `
+Collects library versions in multiple build.gradle(.kts) and generates libs.versions.toml in PATH/gradle.
+If no PATH is provided, the current working directory is used.
+Some manual intervention may be required.
+
+Caution:
+  If libs.version.toml already exists, it will be overwritten.
+`,
 	Args: func(cmd *cobra.Command, args []string) error {
 		if len(args) > 1 {
 			return errors.New("requires at most one arg")
@@ -72,7 +81,6 @@ var generateCommand = &cobra.Command{
 		fmt.Println("!!! DONE !!!")
 		fmt.Printf("Generated: %s", catalogFile.Name())
 	},
-	Short: "Generate libs.versions.toml",
 }
 
 func init() {
