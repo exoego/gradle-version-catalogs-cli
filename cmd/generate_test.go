66b51288cf32512783e59e4255acc1f563e58db8
exoego <ytatsuno.jp@gmail.com>
2025-04-19 08:07:08 +0900

    Test

--- cmd/generate_test.go
+++ cmd/generate_test.go
@@ -6,22 +6,12 @@ import (
 	"testing"
 )
 
-func TestRoot(t *testing.T) {
-	os.Args = []string{"cli"}
-	assert.NoError(t, rootCmd.Execute())
-}
-
-func Test1(t *testing.T) {
+func TestNoArgument(t *testing.T) {
 	os.Args = []string{"cli", "generate"}
-	assert.NoError(t, generateCommand.Execute())
+	assert.ErrorContains(t, generateCommand.Execute(), "not a Gradle project")
 }
 
-func Test2(t *testing.T) {
+func TestExplicitPath(t *testing.T) {
 	os.Args = []string{"cli", "generate", "./path/to/gradle/project"}
-	assert.NoError(t, generateCommand.Execute())
-}
-
-func Test3(t *testing.T) {
-	os.Args = []string{"cli", "generate", "./path/to/gradle/project", "extra-arg"}
-	assert.Error(t, generateCommand.Execute())
+	assert.ErrorContains(t, generateCommand.Execute(), "not a Gradle project")
 }
