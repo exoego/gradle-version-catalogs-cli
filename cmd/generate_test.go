4feafdc65e5c354d1950f16c8772ac5e7a39f164
exoego <ytatsuno.jp@gmail.com>
2025-04-19 07:15:25 +0900

    Test

--- cmd/generate_test.go
+++ cmd/generate_test.go
@@ -1,27 +1,27 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
 )
 
 func TestRoot(t *testing.T) {
+	os.Args = []string{"cli"}
 	assert.NoError(t, rootCmd.Execute())
 }
 
 func Test1(t *testing.T) {
-	assert.Equal(t, 6, len(os.Args), fmt.Sprintf("6 args expected, got %v", os.Args))
-
-	os.Args = append(os.Args, "generate")
-	os.Args = append(os.Args, "./path/to/gradle/project")
+	os.Args = []string{"cli", "generate"}
 	assert.NoError(t, generateCommand.Execute())
 }
 
 func Test2(t *testing.T) {
-	os.Args = append(os.Args, "generate")
-	os.Args = append(os.Args, "./path/to/gradle/project")
-	os.Args = append(os.Args, "extra-arg")
+	os.Args = []string{"cli", "generate", "./path/to/gradle/project"}
+	assert.NoError(t, generateCommand.Execute())
+}
+
+func Test3(t *testing.T) {
+	os.Args = []string{"cli", "generate", "./path/to/gradle/project", "extra-arg"}
 	assert.Error(t, generateCommand.Execute())
 }
