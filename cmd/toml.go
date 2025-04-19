7686e1338e11f55cb2c0949cbf6d85429501f789
exoego <ytatsuno.jp@gmail.com>
2025-04-19 16:57:13 +0900

    Refactor with strings.Builder

--- cmd/toml.go
+++ cmd/toml.go
@@ -1,9 +1,7 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/BurntSushi/toml"
-	"io"
 	"maps"
 	"os"
 	"slices"
@@ -53,189 +51,144 @@ func WriteCatalog(path string, catalog VersionCatalog) error {
 	if err != nil {
 		return err
 	}
-	if err := writeVersions(writer, catalog.Versions); err != nil {
+	if _, err := writer.WriteString(writeVersions(catalog.Versions)); err != nil {
 		return err
 	}
-	if err := writeLibraries(writer, catalog.Libraries); err != nil {
+	if _, err := writer.WriteString(writeLibraries(catalog.Libraries)); err != nil {
 		return err
 	}
-	if err := writeBundles(writer, catalog.Bundles); err != nil {
+	if _, err := writer.WriteString(writeBundles(catalog.Bundles)); err != nil {
 		return err
 	}
-	if err := writePlugins(writer, catalog.Plugins); err != nil {
+	if _, err := writer.WriteString(writePlugins(catalog.Plugins)); err != nil {
 		return err
 	}
 	return writer.Close()
 }
 
-func writeVersions(writer io.StringWriter, versions Versions) error {
+func writeVersions(versions Versions) string {
 	if len(versions) == 0 {
-		return nil
+		return ""
 	}
-	_, err := writer.WriteString(fmt.Sprintf("[versions]%s", LineBreak))
-	if err != nil {
-		return err
-	}
+	var builder strings.Builder
+	builder.WriteString("[versions]")
+	builder.WriteString(LineBreak)
 	for _, k := range slices.Sorted(maps.Keys(versions)) {
-		_, err := writer.WriteString(fmt.Sprintf("%s = %s%s", k, strconv.Quote(versions[k]), LineBreak))
-		if err != nil {
-			return err
-		}
+		builder.WriteString(k)
+		builder.WriteString(" = ")
+		builder.WriteString(strconv.Quote(versions[k]))
+		builder.WriteString(LineBreak)
 	}
-	_, err = writer.WriteString(LineBreak)
-	if err != nil {
-		return err
-	}
-	return nil
+	builder.WriteString(LineBreak)
+	return builder.String()
 }
 
-func writeLibraries(writer io.StringWriter, libraries Libraries) error {
+func writeLibraries(libraries Libraries) string {
 	if len(libraries) <= 0 {
-		return nil
+		return ""
 	}
-	_, err := writer.WriteString(fmt.Sprintf("[libraries]%s", LineBreak))
-	if err != nil {
-		return err
-	}
+	var builder strings.Builder
+	builder.WriteString("[libraries]")
+	builder.WriteString(LineBreak)
 	for _, k := range slices.Sorted(maps.Keys(libraries)) {
 		v := libraries[k]
-		_, err := writer.WriteString(fmt.Sprintf("%s = {", k))
-		if err != nil {
-			return err
-		}
+		builder.WriteString(k)
+		builder.WriteString(" = {")
 		if module, ok := v["module"].(string); ok {
-			_, err := writer.WriteString(fmt.Sprintf(" module = %s", strconv.Quote(module)))
-			if err != nil {
-				return err
-			}
+			builder.WriteString(" module = ")
+			builder.WriteString(strconv.Quote(module))
 		} else if group, ok := v["group"].(string); ok {
-			_, err := writer.WriteString(fmt.Sprintf(" group = %s", strconv.Quote(group)))
-			if err != nil {
-				return err
-			}
+			builder.WriteString(" group = ")
+			builder.WriteString(strconv.Quote(group))
 			if name, ok := v["name"].(string); ok {
-				_, err := writer.WriteString(fmt.Sprintf(", name = %s", strconv.Quote(name)))
-				if err != nil {
-					return err
-				}
+				builder.WriteString(", name = ")
+				builder.WriteString(strconv.Quote(name))
 			}
 		}
-
-		err = writeVersionEntry(writer, v["version"])
-		if err != nil {
-			return err
-		}
-
-		_, err = writer.WriteString(fmt.Sprintf(" }%s", LineBreak))
-		if err != nil {
-			return err
-		}
+		builder.WriteString(writeVersionEntry(v["version"]))
+		builder.WriteString(" }")
+		builder.WriteString(LineBreak)
 	}
-	_, err = writer.WriteString(LineBreak)
-	if err != nil {
-		return err
-	}
-	return nil
+	builder.WriteString(LineBreak)
+	return builder.String()
 }
 
-func writeVersionEntry(writer io.StringWriter, versionContainer any) error {
+func writeVersionEntry(versionContainer any) string {
 	if version, ok := versionContainer.(string); ok {
-		_, err := writer.WriteString(fmt.Sprintf(", version = %s", strconv.Quote(version)))
-		if err != nil {
-			return err
-		}
-	} else if version, ok := versionContainer.(map[string]any); ok {
-		if ref, ok := version["ref"].(string); ok && len(version) == 1 {
-			_, err := writer.WriteString(fmt.Sprintf(", version.ref = %s", strconv.Quote(ref)))
-			if err != nil {
-				return err
-			}
-
-		} else {
-			_, err := writer.WriteString(", version = { ")
-			if err != nil {
-				return err
-			}
-
-			written := false
-			sep := ""
-			for _, vk := range []string{"ref", "strictly", "prefer", "require", "reject"} {
-				if v, ok := version[vk].(string); ok {
-					if written {
-						sep = ", "
-					}
-					written = true
-					_, err := writer.WriteString(fmt.Sprintf("%s%s = %s", sep, vk, strconv.Quote(v)))
-					if err != nil {
-						return err
-					}
-				}
-			}
-			if rejectAll, ok := version["rejectAll"].(bool); ok {
-				_, err := writer.WriteString(fmt.Sprintf(", rejectAll = %v", rejectAll))
-				if err != nil {
-					return err
-				}
-			}
-			_, err = writer.WriteString(" }")
-			if err != nil {
-				return err
-			}
-		}
+		var builder strings.Builder
+		builder.WriteString(", version = ")
+		builder.WriteString(strconv.Quote(version))
+		return builder.String()
 	}
-	return nil
+	if version, ok := versionContainer.(map[string]any); ok {
+		var builder strings.Builder
+		if ref, ok := version["ref"].(string); ok && len(version) == 1 {
+			builder.WriteString(", version.ref = ")
+			builder.WriteString(strconv.Quote(ref))
+			return builder.String()
+		}
+		builder.WriteString(", version = { ")
+
+		written := false
+		for _, vk := range []string{"ref", "strictly", "prefer", "require", "reject"} {
+			if v, ok := version[vk].(string); ok {
+				if written {
+					builder.WriteString(", ")
+				}
+				written = true
+				builder.WriteString(vk)
+				builder.WriteString(" = ")
+				builder.WriteString(strconv.Quote(v))
+			}
+		}
+		if rejectAll, ok := version["rejectAll"].(bool); ok {
+			builder.WriteString(", rejectAll = ")
+			builder.WriteString(strconv.FormatBool(rejectAll))
+		}
+		builder.WriteString(" }")
+		return builder.String()
+	}
+	return ""
 }
 
-func writeBundles(writer io.StringWriter, bundles Bundles) error {
+func writeBundles(bundles Bundles) string {
 	if len(bundles) == 0 {
-		return nil
+		return ""
 	}
-	_, err := writer.WriteString(fmt.Sprintf("[bundles]%s", LineBreak))
-	if err != nil {
-		return err
-	}
+	var builder strings.Builder
+	builder.WriteString("[bundles]")
+	builder.WriteString(LineBreak)
 	for _, k := range slices.Sorted(maps.Keys(bundles)) {
 		v := bundles[k]
 		quoted := make([]string, len(v))
 		for i, s := range v {
 			quoted[i] = strconv.Quote(s)
 		}
-		_, err := writer.WriteString(fmt.Sprintf("%s = [%s]%s", k, strings.Join(quoted, ", "), LineBreak))
-		if err != nil {
-			return err
-		}
+		builder.WriteString(k)
+		builder.WriteString(" = [")
+		builder.WriteString(strings.Join(quoted, ", "))
+		builder.WriteString("]")
+		builder.WriteString(LineBreak)
 	}
-	_, err = writer.WriteString(LineBreak)
-	if err != nil {
-		return err
-	}
-	return nil
+	builder.WriteString(LineBreak)
+	return builder.String()
 }
 
-func writePlugins(writer io.StringWriter, plugins Plugins) error {
+func writePlugins(plugins Plugins) string {
 	if len(plugins) == 0 {
-		return nil
+		return ""
 	}
-	_, err := writer.WriteString(fmt.Sprintf("[plugins]%s", LineBreak))
-	if err != nil {
-		return err
-	}
+	var builder strings.Builder
+	builder.WriteString("[plugins]")
+	builder.WriteString(LineBreak)
 	for _, k := range slices.Sorted(maps.Keys(plugins)) {
 		plugin := plugins[k]
-		_, err := writer.WriteString(fmt.Sprintf("%s = { id = %s", k, strconv.Quote(plugin.Id)))
-		if err != nil {
-			return err
-		}
-
-		err = writeVersionEntry(writer, plugin.Version)
-		if err != nil {
-			return err
-		}
-
-		_, err = writer.WriteString(fmt.Sprintf(" }%s", LineBreak))
-		if err != nil {
-			return err
-		}
+		builder.WriteString(k)
+		builder.WriteString(" = { id = ")
+		builder.WriteString(strconv.Quote(plugin.Id))
+		builder.WriteString(writeVersionEntry(plugin.Version))
+		builder.WriteString(" }")
+		builder.WriteString(LineBreak)
 	}
-	return nil
+	return builder.String()
 }
