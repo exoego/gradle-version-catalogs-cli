60d4e2ecaf655b07cbc929601a3f53f838e35a8f
exoego <ytatsuno.jp@gmail.com>
2025-04-19 11:11:27 +0900

    Add more

--- cmd/generate_test.go
+++ cmd/generate_test.go
@@ -24,6 +24,34 @@ func TestNoErrorIfGradleDirectory(t *testing.T) {
 	os.Args = []string{"cli", "generate", tempdir}
 	assert.NoError(t, generateCommand.Execute())
 
-	f, _ := os.ReadFile(filepath.Join(tempdir, "libs.versions.toml"))
+	f, _ := os.ReadFile(filepath.Join(tempdir, "gradle", "libs.versions.toml"))
 	assert.Equal(t, string(f), "", "Generates an empty libs.versions.toml")
+}
+
+func TestSkipTopLevelSettingsFile(t *testing.T) {
+	tempdir := t.TempDir()
+	writeFile(t, tempdir, "gradle/wrapper/dummy.txt", "")
+	writeFile(t, tempdir, "build.gradle", `
+		api("foo:foo:1.0")
+	`)
+	writeFile(t, tempdir, "settings.gradle.kts", `
+		implementation("ignore:ignore:1.0")
+	`)
+	writeFile(t, tempdir, "foo/build.gradle", `
+		testImplementation("bar:bar:0.1")
+	`)
+	writeFile(t, tempdir, "foo/settings.gradle.kts", `
+		implementation("ok:ok:2.0")	
+	`)
+
+	os.Args = []string{"cli", "generate", tempdir}
+	assert.NoError(t, generateCommand.Execute())
+
+	f, _ := os.ReadFile(filepath.Join(tempdir, "gradle", "libs.versions.toml"))
+	assert.Equal(t, string(f), `[libraries]
+bar-bar = { group = "bar", name = "bar", version = "0.1" }
+foo-foo = { group = "foo", name = "foo", version = "1.0" }
+ok-ok = { group = "ok", name = "ok", version = "2.0" }
+
+`, "Generates an empty libs.versions.toml")
 }
