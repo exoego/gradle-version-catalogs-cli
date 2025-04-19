da9e5b5c64ea98cab7210ec39a4fa29c36f97360
exoego <ytatsuno.jp@gmail.com>
2025-04-19 17:20:07 +0900

    Test rootCmd

--- cmd/root.go
+++ cmd/root.go
@@ -9,19 +9,14 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use: "gradle-version-catalogs-cli",
-}
-
-func exitError(msg interface{}) {
-	fmt.Fprintln(os.Stderr, msg)
-	os.Exit(1)
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return cmd.Help()
+	},
 }
 
 func Execute() {
-	rootCmd.Run = func(cmd *cobra.Command, args []string) {
-		_ = rootCmd.Help()
-	}
-
 	if err := rootCmd.Execute(); err != nil {
-		exitError(err)
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
