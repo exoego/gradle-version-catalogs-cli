ae5a0a0009ae731a9ce4c868e0c15041851ea778
TATSUNO “Taz” Yasuhiro <ytatsuno.jp@gmail.com>
2025-04-19 17:36:31 +0900

    More tests (#21)

--- cmd/version_test.go
+++ cmd/version_test.go
@@ -1,19 +1,47 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/stretchr/testify/assert"
+	"os"
 	"runtime"
 	"testing"
 )
 
+func CaptureStdout(t *testing.T, process func() error) (string, error) {
+	t.Helper()
+	original := os.Stdout
+	defer func() {
+		os.Stdout = original
+	}()
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+	err := process()
+	if err != nil {
+		return "", err
+	}
+	err = w.Close()
+	if err != nil {
+		return "", err
+	}
+	var buffer bytes.Buffer
+	if _, err := buffer.ReadFrom(r); err != nil {
+		return "", err
+	}
+	s := buffer.String()
+	return s[:len(s)-1], nil
+}
+
 func TestGetVersion(t *testing.T) {
-	got := getVersion()
+	os.Args = []string{"cli", "version"}
+	stdout, err := CaptureStdout(t, generateCommand.Execute)
+	assert.NoError(t, err)
+
 	want := fmt.Sprintf(`Version: %s
 Revision: %s
 OS: %s
 Arch: %s`, VersionString, Revision, runtime.GOOS, runtime.GOARCH)
 
-	if want != got {
-		t.Fatalf("unexpected version info. want: %s, got: %s", want, got)
-	}
+	assert.Equal(t, want, stdout)
 }
